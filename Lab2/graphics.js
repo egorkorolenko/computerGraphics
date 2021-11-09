bc2ce87cfddb3151469ba5888c1fb3b0212d9600
Egor Korolenko <korolenko.egor39@gmail.com>
2021-11-09 18:20:19 +0600

    Алгоритм Брезенхама

---
+++ Lab2/graphics.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext('2d')
+
+var inp = prompt("Введите координаты начальной и конечной точки");
+var input = inp.split(' ')
+line(parseInt(input[0]),parseInt(input[1]),parseInt(input[2]),parseInt(input[3]))
+
+function line(x0, y0, x1, y1) {
+    var dx = Math.abs(x1 - x0);
+    var dy = Math.abs(y1 - y0);
+    var sx = (x0 < x1) ? 1 : -1;
+    var sy = (y0 < y1) ? 1 : -1;
+    var err = dx - dy;
+
+    while(true) {
+        ctx.fillRect(x0,y0,1,1)
+        if ((x0 === x1) && (y0 === y1))
+            break;
+
+        let e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x0  += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0  += sy;
+        }
+    }
+}
