4534d0fea7acb6020c2048eb3e2032f78e8ff377
Egor Korolenko <korolenko.egor39@gmail.com>
2021-11-29 19:57:10 +0600

    3 лаба

--- Lab3/graphics.js
+++ Lab3/graphics.js
@@ -4,16 +4,19 @@ var ctx = canvas.getContext('2d')
 var inp = prompt("Введите координаты начальной и конечной точки (4 числа через пробел)");
 var input = inp.split(' ')
 line(parseInt(input[0]),parseInt(input[1]),parseInt(input[2]),parseInt(input[3]))
+ctx.scale(8, 8)
 
-function line(x0, y0, x1, y1) {
+async function line(x0, y0, x1, y1) {
     var dx = Math.abs(x1 - x0);
     var dy = Math.abs(y1 - y0);
     var sx = (x0 < x1) ? 1 : -1;
     var sy = (y0 < y1) ? 1 : -1;
     var err = dx - dy;
 
-    while(true) {
-        ctx.fillRect(x0,y0,1,1)
+    while (true) {
+
+        await sleep(200)
+        ctx.fillRect(x0, y0, 1, 1)
         if ((x0 === x1) && (y0 === y1))
             break;
 
@@ -21,11 +24,15 @@ function line(x0, y0, x1, y1) {
 
         if (e2 > -dy) {
             err -= dy;
-            x0  += sx;
+            x0 += sx;
         }
         if (e2 < dx) {
             err += dx;
-            y0  += sy;
+            y0 += sy;
         }
     }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
